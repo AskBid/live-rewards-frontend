5af6f541e83eaba6b07d5ae4b77a2c8f16cc4836
Sergio Marelli <sergio.mrll@gmail.com>
2021-03-23 12:31:58 +0000

    complete form first fetch draft

--- src/containers/NewUserForm.js
+++ src/containers/NewUserForm.js
@@ -1,50 +1,67 @@
 import React, { Component } from 'react'
 
 export default class NewUserForm extends Component {
+
+    fetchOnSubmit = (e) => {
+      e.preventDefault()
+      const form = e.target
+      const body = new FormData()
+
+      body.append('user[username]', form.username.value)
+      body.append('user[password]', form.password.value)
+      body.append('user[email]', form.email.value)
+      body.append('user[stake_address]', form.stake_address.value)
+      console.log(Array.from(body)) 
+      fetch('http://localhost:3001/users', {
+        method: 'POST',
+        body
+      }).then(resp => resp.json())
+        .then(json => {
+          console.log(json);
+          this.props.history.push('/')
+        })
+    }
+
     render() {
         return (
-          <form>
-            <label htmlFor="name" className="block">
+          <form onSubmit={this.fetchOnSubmit}>
+            <label htmlFor="username" className="block">
               Username*
               <span className="text-red-400"></span>
             </label>
             <input
               type="text"
-              name="name"
-              id="name"
+              name="username"
               className='w-100 border border-primary shadow p-2 mb-4 rounded-3'>
             </input>
-            <label htmlFor="name" className="block">
+            <label htmlFor="password" className="block">
               Password*
               <span className="text-red-400"></span>
             </label>
             <input
-              type="text"
-              name="name"
-              id="name"
+              type="password"
+              name="password"
               className='w-100 border border-primary shadow p-2 mb-5 rounded-3'>
             </input>
-            <label htmlFor="name" className="block">
+            <button className='w-100 h-1 border border-primary rounded-pill mb-5' type='Submit'>Submit</button>
+            <label htmlFor="email" className="block">
               Email?
               <span className="text-red-400"></span>
             </label>
             <input
               type="text"
-              name="name"
-              id="name"
+              name="email"
               className='w-100 border border-primary shadow p-2 mb-4 rounded-3'>
             </input>
-            <label htmlFor="name" className="block">
+            <label htmlFor="stake_address" className="block">
               Stake Address?
               <span className="text-red-400"></span>
             </label>
             <input
               type="text"
-              name="name"
-              id="name"
+              name="stake_address"
               className='w-100 border border-primary shadow p-2 mb-5 rounded-3'>
             </input>
-            <button className='w-100 h-1 border border-primary rounded-pill' type='Submit'>Submit</button>
           </form>
         )
     }

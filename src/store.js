3bc9ddb1f4b0dd6b53f7eda01c4aab15a399d080
Sergio Marelli <sergio.mrll@gmail.com>
2021-03-24 14:50:40 +0000

    add redux-devtools to createStore

--- src/store.js
+++ src/store.js
@@ -1,14 +1,13 @@
 // store.js
 // import { createStore, applyMiddleware } from "redux";
 import { createStore } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension";
 // import thunk from "redux-thunk";
 import rootReducer from "./reducers/index";
 
 const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  // composeWithDevTools(applyMiddleware(thunk))
+  rootReducer, composeWithDevTools()
+  // rootReducer, composeWithDevTools(applyMiddleware(thunk))
 );
 
 export default store;

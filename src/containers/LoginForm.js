12987c551b2cb01ffe6d301fd147f1b490fe4a61
Sergio Marelli <sergio.mrll@gmail.com>
2021-03-29 00:55:27 +0100

    fix some styling

--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -36,7 +36,7 @@ class LoginForm extends Component {
                 <img alt="spinner" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
               }
             </h2>
-            <div class="row">
+            <div>
               <fieldset className='ml-auto mr-auto w-100'>
                 <label htmlFor="username" className="block">
                   Username*
@@ -50,7 +50,7 @@ class LoginForm extends Component {
                 </input>
               </fieldset>
             </div>
-            <div class="row">
+            <div>
               <fieldset className='ml-auto mr-auto w-100'>
                 <label htmlFor="password" className="block">
                   Password*
@@ -64,10 +64,10 @@ class LoginForm extends Component {
                 </input>
               </fieldset>
             </div>
-            <div class="row  mb-5">
-                <div class="col d-flex"><button className='h-1 border-0 rounded-pill ml-auto mr-auto' 
+            <div class="row mb-5 ml-2 mr-2 mt-3">
+                <div class="col-8 d-flex"><button className='h-1 border-0 rounded-pill ml-auto mr-auto' 
                   style={{width:'100%',display:'inline'}} type='Submit'>Login</button></div>
-                <div class="col d-flex">
+                <div class="col-4 d-flex">
                   <Link to="/Signup" className='ml-auto mr-auto mt-auto mb-auto text-primary hardlink'>Register</Link>
                 </div>
             </div>
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -6,7 +6,7 @@ function Signin({history}) {
     <div className="container">
       <div className="row mb-5">
         <div className='col'></div>
-        <div className="col-lg-6  mt-5">
+        <div className="col-lg-6 mt-5">
           <LoginForm history={history}/>
         </div>
         <div className='col'></div>

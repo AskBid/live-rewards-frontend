0ae71dd5134112261dea0cead7fd55c7a2405b59
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-07 21:26:08 +0100

    add delete button style-component

---
+++ src/components/StakeTab/DeleteButton.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import {FaTimes} from 'react-icons/fa'
+
+export const DeleteBtn = styled.button `
+	background: rgba(245, 81, 66, 0.05);
+	border: none;
+	color: white;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: rgba(245, 81, 66, 0.8);
+    color: #e4e2ff;
+    border: none;
+  }
+`;
+
+export const CloseIcon = styled(FaTimes)`
+	color: rgba(255, 250, 250, 0.8);
+`

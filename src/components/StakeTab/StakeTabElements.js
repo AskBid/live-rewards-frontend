08d170b0f38d5f8dec3f248992ae5f0e56c13ad2
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-08 10:20:52 +0100

    add style for address label

---
+++ src/components/StakeTab/StakeTabElements.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+import {FaTimes} from 'react-icons/fa'
+
+export const DeleteBtn = styled.button `
+	background: rgba(245, 81, 66, 0.05);
+	border: none;
+	color: white;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: rgba(245, 81, 66, 0.8);
+    color: #e4e2ff;
+    border: none;
+  }
+`;
+
+export const CloseIcon = styled(FaTimes)`
+	color: rgba(255, 250, 250, 0.8);
+	margin: 0px;
+	padding: 0px;
+`
+
+export const AddrLabel = styled.span`
+	color: rgba(40, 40, 40, 0.3);
+	font-size: 0.6em;
+	position: absolute;
+	top: -11px;
+	right: 0px;
+	&:hover {
+    color: rgba(245, 81, 66, 0.7);
+    font-weight: bold;
+    cursor: auto; 
+  }
+`

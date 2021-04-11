ef146537a794d4bc4693d44477f4dd9e06046ba9
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-11 22:58:29 +0100

    preparing component to use actions and reducers for pool-compare

--- src/containers/ProjectedStakesColumn.js
+++ src/containers/ProjectedStakesColumn.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
 import { getPoolCompareUserEpochStakes } from '../actions/epoch_stake.actions';
+import { getEpochStake } from '../actions/epoch_stake.actions';
 import EpochTab from '../components/EpochTab';
 import { groupBy } from 'underscore';
 
 class ProjectedStakesColumn extends Component {
 
   componentDidMount() {
-    debugger
     if (!this.props.history.location.pathname.includes('/pools/new')) {
       const { username, epoch_stake_id } = this.props.match.params
-      this.props.getPoolCompareUserEpochStakes(username, epoch_stake_id)
+      !this.props.epoch_stake && 
+        getUrlEpochStake(epoch_stake_id)
+      !this.props.projected_stakes && 
+        this.props.getPoolCompareUserEpochStakes(username, epoch_stake_id)
     }
+  }
+
+  getUrlEpochStake = (epoch_stake_id) => {
+    epoch_stake = epoch_stakes.filter((epoch_stake) epoch_stake.id === epoch_stake_id)
+    !epoch_stake && getEpochStake(epoch_stake_id)
   }
 
   deployEpochs = () => {
   }
 
   render() {
+    console.log(this.props.epoch_stake)
+    console.log(this.props.epoch_stakes)
+    console.log(this.props.projected_stakes)
     return (
       <React.Fragment>
         <div className="d-flex flex-wrap">
@@ -39,12 +50,15 @@ class ProjectedStakesColumn extends Component {
 const mapDispatchToProps = dispatch => {
   return {
     getPoolCompareUserEpochStakes: (username, epoch_stake_id) => dispatch(getPoolCompareUserEpochStakes(username, epoch_stake_id))
+    getEpochStake: (epoch_stake_id) => dispatch(getEpochStake(epoch_stake_id))
   }
 }
 
 const mapStateToProps = store => {
   return {
-    epoch_stakes: store.epoch_stakes
+    epoch_stakes: store.epoch_stakes.list
+    epoch_stake: store.epoch_stakes.epoch_stake
+    projected_stakes: store.projected_stakes.list
   }
 }
 

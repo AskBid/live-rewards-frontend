a4d33ac35e57886f7d713053728aa9e57dc9c19d
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-11 22:56:55 +0100

    add actions to #show epoch_stakes/epoch_stake_id

--- src/actions/epoch_stake.actions.js
+++ src/actions/epoch_stake.actions.js
@@ -4,7 +4,10 @@ import {
   REQUEST_USER_EPOCH_STAKES_FAILURE,
   REQUEST_USER_POOL_HASHES_EPOCH_STAKES,
 	REQUEST_USER_POOL_HASHES_EPOCH_STAKES_SUCCESS,
-	REQUEST_USER_POOL_HASHES_EPOCH_STAKES_FAILURE
+	REQUEST_USER_POOL_HASHES_EPOCH_STAKES_FAILURE,
+	REQUEST_EPOCH_STAKE,
+	REQUEST_EPOCH_STAKE_SUCCESS,
+	REQUEST_EPOCH_STAKE_FAILURE
 } from '.'
 import { authHeader } from '../helpers/auth-header'
 
@@ -52,8 +55,7 @@ export const getPoolCompareUserEpochStakes = (username, epoch_stake_id) => {
 					return res.json.then(json => Promise.reject())
 				}
 			})
-	  	.then(json => { 
-	  		console.log(json)
+	  	.then(json => {
 	  		dispatch({
 	  			type: REQUEST_USER_POOL_HASHES_EPOCH_STAKES_SUCCESS, 
 	  			payload: json
@@ -61,6 +63,34 @@ export const getPoolCompareUserEpochStakes = (username, epoch_stake_id) => {
 	  	})
 			.catch(err => {
 				dispatch({type: REQUEST_USER_POOL_HASHES_EPOCH_STAKES_FAILURE})
+			})
+	}
+}
+
+export const getEpochStake = (epoch_stake_id) => {
+	return (dispatch) => {
+		dispatch({type: REQUEST_EPOCH_STAKE})
+		return fetch(`http://localhost:3001/epoch_stakes/${epoch_stake_id}`, {
+			method: 'GET',
+	    headers: {
+	    	'Content-Type': 'application/json',
+	    	"Accept": "application/json"
+	    }
+	  }).then(res => {
+				if (res.ok) {
+					return res.json()
+				} else {
+					return res.json.then(json => Promise.reject())
+				}
+			})
+	  	.then(json => {
+	  		dispatch({
+	  			type: REQUEST_EPOCH_STAKE_SUCCESS, 
+	  			payload: json
+	  		});
+	  	})
+			.catch(err => {
+				dispatch({type: REQUEST_EPOCH_STAKE_FAILURE})
 			})
 	}
 }

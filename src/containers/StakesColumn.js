285c97e580c66ed1db6c53b78fce81ac992a5fd1
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-14 12:22:55 +0100

    add message to live-rewards when there is not user_stake_address

--- src/containers/StakesColumn.js
+++ src/containers/StakesColumn.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { userEpochStakes } from '../actions/epoch_stake.actions';
 import EpochTab from '../components/EpochTab';
 import { groupBy } from 'underscore';
+import { Link } from 'react-router-dom'
 
 class StakesColumn extends Component {
 
@@ -24,6 +25,23 @@ class StakesColumn extends Component {
     } else { return undefined }
   }
 
+  textIfEmpty = () => {
+    if (this.props.epoch_stakes.length === 0) {
+      return (
+        <div className='text-muted rounded pt-2 pl-2 pr-2 pb-4 shadow' style={{background:'rgba(255, 255, 255,0.5)'}}>
+          <p className='text-muted mb-5 mt-4 ml-5 mr-5'>
+            You haven't entered any <b>Stake Address</b>.<br/><br/>
+            To view your rewards for the last 3 epochs add your <b>Stake Address</b> by clicking <Link to={`/live-rewards/users/${this.props.username}/user_stakes/new`} className='hardlink'>here</Link>.
+            <br/><br/>
+            If you don't know how to find your stake address see visit the <Link to={`/howto`} className='hardlink grey'>How To</Link>.
+            <br/><br/>
+            To follow a random stake click <Link to={`/howto`} className='hardlink grey'>Random Stake!</Link>.
+          </p>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -42,6 +60,7 @@ class StakesColumn extends Component {
             </div>
           }
         </div>
+        {this.textIfEmpty()}
         {this.deployEpochs()}
       </React.Fragment>
     )

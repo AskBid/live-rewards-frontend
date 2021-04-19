4686a2dceed0a8435be474a0015bb5d3d027a4f6
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-19 20:06:37 +0100

    add color change for progress bar

--- src/components/EpochTab.js
+++ src/components/EpochTab.js
@@ -4,32 +4,50 @@ import Moment from 'moment'
 
 const EpochTab = ({epochno, stakes, buttonsOff}) => {
 
-  const epoch_end_date = (epochno) => {
-    const m = Moment('2020-08-13T21:44:51.000')
-    m.add(5 * (260-210), 'days')
-    return {date: m.format("ddd Do MMM"), time: m.format("hh:mmA")}
-  }
+  const epoch_end_date = ((epochno) => {
+    const reward_pay = Moment('2020-08-13T21:44:51.000')
+    reward_pay.add(5 * ((epochno+1) - 210), 'days')
 
-  const epoch_status = () => {
+    const epoch_end = Moment('2020-08-13T21:44:51.000')
+    epoch_end.add(5 * ((epochno) - 210), 'days')
+
+    const sec_left = epoch_end.unix() - Moment().unix()
+    const five_days_sec = 432000
+    const epoch_progress = sec_left > 0 ? ((five_days_sec-sec_left) / five_days_sec) * 100 : 100
+
+    return {
+      date: reward_pay.format("ddd Do MMM"), 
+      time: reward_pay.format("hh:mmA"),
+      epoch_progress: epoch_progress,
+    }
+  })(epochno)
+
+  const epoch_status = (() => {
     const epoch_passed = stakes[0].epoch_info.current_epoch - epochno
     switch (epoch_passed) {
       case 0:
         return {
           rewards: 'ongoing', 
-          epoch: 'in progress'
+          epoch: 'in progress',
+          color: 'warning',
+          opacity: '45%'
         }
       case 1:
         return {
           rewards: 'calculating', 
-          epoch: 'ended'
+          epoch: 'ended',
+          color: 'info',
+          opacity: '55%'
         }
       default:
         return {
           rewards: 'received',
-          epoch: 'ended'
+          epoch: 'ended',
+          color: 'info',
+          opacity: '90%'
         }
     }
-  } 
+  })()
 
   return (
       <div className='text-light bg-white bg-gradient rounded pt-4 shadow mb-5'>
@@ -49,7 +67,7 @@ const EpochTab = ({epochno, stakes, buttonsOff}) => {
                   </div>
                   <div className='row mb-2 mt-0'>
                     <div className='col min-vw-10 pl-auto pr-auto text-muted text-center text-nowrap'>
-                      <b>{ epoch_status()['rewards'] }</b>
+                      <b>{ epoch_status.rewards }</b>
                     </div>
                   </div>
                   <div className='row'>
@@ -59,10 +77,10 @@ const EpochTab = ({epochno, stakes, buttonsOff}) => {
                   </div>
                   <div className='row'>
                     <div className='date col min-vw-10 pl-auto pr-auto text-right text-muted text-center text-nowrap'>
-                      {epoch_end_date(epochno).date}
+                      {epoch_end_date.date}
                     </div>
                     <div className='date col min-vw-10 pl-auto pr-auto text-right text-muted text-center text-nowrap'>
-                      {epoch_end_date(epochno).time}
+                      {epoch_end_date.time}
                     </div>
                   </div>
                 </div>
@@ -71,8 +89,8 @@ const EpochTab = ({epochno, stakes, buttonsOff}) => {
           </div>
           {stakes.map(stake => <StakeTab stake={stake} buttonsOff={buttonsOff}/>)}
         </div>
-        <div className="progress mt-2" style={{height: "3px"}}>
-          <div className="progress-bar bg-info" role={'progressbar'} style={{width: '60%'}} aria-valuenow={"50"} aria-valuemin={"0"} aria-valuemax={"100"}></div>
+        <div className="progress mt-2" style={{height: "4.5px", opacity: `${epoch_status.opacity}`}}>
+          <div className={`progress-bar bg-${epoch_status.color}`} role={'progressbar'} style={{width: `${epoch_end_date.epoch_progress}%`}} aria-valuenow={"50"} aria-valuemin={"0"} aria-valuemax={"100"}></div>
         </div>
       </div>
   )

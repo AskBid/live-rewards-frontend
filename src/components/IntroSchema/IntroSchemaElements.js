bd2915a731499fe636d11205c4085252fb0426d1
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-21 11:50:38 +0100

    background change

--- src/components/IntroSchema/IntroSchemaElements.js
+++ src/components/IntroSchema/IntroSchemaElements.js
@@ -4,22 +4,22 @@ import { NavLink as Link } from 'react-router-dom'
 export const LinkText = styled(Link) `
 
   &:link {
-    color: #42c0c9;
+    color: #18c7c4;
     text-decoration: none;
   }
 
   &:visited {
-    color: #42c0c9;
+    color: #18c7c4;
     text-decoration: none;
   }
 
   &:active {
-    color: #42c0c9;
+    color: #18c7c4;
     text-decoration: none;
   }
 
   &:hover {
-    color: #42c0c9;
+    color: #18c7c4;
     text-decoration: none;
   }
 `;

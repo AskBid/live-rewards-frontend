6a6d0238929be18e60014ae0b615264e71aca5db
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-21 10:05:00 +0100

    draft action for gecko price request

--- src/actions/session.actions.js
+++ src/actions/session.actions.js
@@ -6,8 +6,8 @@ import {
 	ERROR,
 	CLEAR_EPOCH_STAKES,
 	REQUEST_PRICE,
-	REQUEST_SUCCESS,
-	REQUEST_FAILURE
+	REQUEST_PRICE_SUCCESS,
+	REQUEST_PRICE_FAILURE
 } from '.'
 
 export const login = (formData) => {
@@ -48,5 +48,37 @@ export const logout = () => {
 		localStorage.removeItem('user')
 		dispatch({type: CLEAR_EPOCH_STAKES})
 		dispatch({type: LOGOUT})
+	}
+}
+
+
+export const getPrice = (symbol) => {
+	return (dispatch) => {
+		dispatch({type: REQUEST_PRICE})
+		return fetch(`https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=${symbol}`, {
+	  	method: 'GET',
+	    headers: {
+	    	'Content-Type': 'application/json',
+	    	"Accept": "application/json"
+	    }
+	  }).then(res => {
+		  	if (res.ok) {
+		  		return res.json()
+		  	} else {
+		  		return res.json().then(gecko => Promise.reject(gecko.errors))
+		  	}
+		  })
+	  	.then((user) => {
+	  		dispatch({
+	  			type: REQUEST_PRICE_SUCCESS,
+	  			user: user.username
+	  		})
+	  		return user
+	  	})
+			.catch(err => {
+				dispatch({type: REQUEST_PRICE_FAILURE})
+				dispatch({type: ERROR, message: err})
+				return Promise.reject(err)
+			})
 	}
 }

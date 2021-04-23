55f5c7c02b4859965e42325be3d839f3fbc410a4
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-24 00:12:02 +0100

    add google analytics and correct texdt in howto

--- src/pages/howto.js
+++ src/pages/howto.js
@@ -11,18 +11,19 @@ function Howto({history}) {
         <div className="col-lg-6 mt-5 mb-5 text-dark">
           <h2 className='text-dark'>All you need is your Stake Address</h2>
           <br/>
-          <p>Your wallet can have many receiving addresses to which you can receive ADA. Those addresses usually start with <i>addr1.</i></p>
-          <p>There is also a unique Stake Address that your wallet uses to delegate to a staking pool. This address starts with <i>stake1.</i></p>
+          <p>Your wallet can have many receiving addresses to which ADA can be sent. Those addresses usually start with <i>addr1.</i></p>
+          <p>There is also a unique Stake Address that your wallet uses to delegate to staking pools. This address starts with <i>stake1.</i></p>
 
-          <p>To find your Stake Address, get any of your receiveing addresses (starting <i>addr1...</i>) and look it up on <a className='hardlink' href="https://cardanoscan.io/" target="_blank" rel="noreferrer">cardanoscan.io</a>.</p> <p>In the <i>Controlled Stake Section</i> you will find the BECH32 Stake Address that you need to see your Rewards in <Link to={`/live-rewards`} className='hardlink'>Live-Rewards</Link>.</p>
+          <p>To find your Stake Address, get any of your receiveing addresses (starting <i>addr1...</i>) and look it up on <a className='hardlink' href="https://cardanoscan.io/" target="_blank" rel="noreferrer">cardanoscan.io</a>.</p> 
+          <p>In the <i>Controlled Stake Section</i>, of the cardanoscan.io page, you will find the BECH32 Stake Address needed to track your Rewards with <Link to={`/live-rewards`} className='hardlink'>Live-Rewards</Link>.</p>
           <br/>
           <div className='container center'>
           <img className='text-center' src={Image} alt="cardanoscan.io stake address location." style={{width:'500px'}}/>
           </div>
           <br/>
-          <p>Add your stake address in the <Link to={`/live-rewards`} className='hardlink'>Live-Rewards</Link> section and magically you'll be able to see the rewards of the current epoch and the two previous epochs.</p>
+          <p>Add your stake address in <Link to={`/live-rewards`} className='hardlink'>Live-Rewards</Link> and magically you'll be able to see the rewards of the last three epochs, including rewards you have not received yet.</p>
 
-          <p>Each delegation has a button that will allow you to compare your delegation with different Pools. So that you can compare the reward performance of your pool of choice.</p>
+          <p>Each epoch stake tab has a button that will allow you to compare your delegation with different Pools. With that you can compare your pool performance with that of other pools.</p>
         </div>
         <div className='col'></div>
       </div>

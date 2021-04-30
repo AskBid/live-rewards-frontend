8769945135f578fe1cc923fb1648245b68a7aab1
Sergio Marelli <sergio.mrll@gmail.com>
2021-05-01 00:21:18 +0100

    group performances by pool and list them

--- src/pages/pools-gauge.js
+++ src/pages/pools-gauge.js
@@ -1,15 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getPoolPerformances } from '../actions/pool.actions'
 import { useSelector, useDispatch } from 'react-redux'
+import { groupBy } from 'underscore'
 
 function PoolsGauge({match}) {
 	const performances = useSelector( state => state.pools.performances )
 	const dispatch = useDispatch()
 
+	// const [isVisible, setVisiblity] = useState();
+
 	useEffect(() => {
     window.scrollTo(0, 0);
     dispatch(getPoolPerformances(20))
   }, []);
+
+  // useEffect(() => {
+  // }, [performances]);
+
+  const deployPerformances = () => {
+    const by_pool_hash = groupBy(performances, 'pool_hash_id')
+    return Object.keys(by_pool_hash).map((pool_hash_id) => {
+    	return (
+	    	<div className='row' key={pool_hash_id}>
+	    		<div className='col'>{pool_hash_id}</div>
+	    		<div className='col d-flex flex-inline'>
+	    			{by_pool_hash[pool_hash_id].map(epoch => <span className='text-nowrap'>{epoch.blocks_delta_pc}{' || '}</span>)}
+	    		</div>
+	    	</div>
+    	)
+    })
+  }
 
   return (
     <div className="container-fluid mh-100">
@@ -17,7 +37,7 @@ function PoolsGauge({match}) {
         <div className='col'>
         </div>
         <div className="col-lg-10 mr-auto ml-auto">
-        	{performances.map(performance => <p>{performance.blocks_delta_pc}</p>)}
+        	{deployPerformances()}
         </div>
         <div className='col'></div>
       </div>

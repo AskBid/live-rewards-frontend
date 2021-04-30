97b2de6a6eeb92e5d5b2a7ae1b4508a7da313ebd
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-30 23:54:16 +0100

    working component queries pools performances

--- src/pages/pools-gauge.js
+++ src/pages/pools-gauge.js
@@ -1,24 +1,28 @@
 import React, { useEffect } from 'react'
-// import StakesColumn from '../containers/StakesColumn'
-// import LiveRewardsControlBar from '../containers/LiveRewardsControlBar'
+import { getPoolPerformances } from '../actions/pool.actions'
+import { useSelector, useDispatch } from 'react-redux'
 
 function PoolsGauge({match}) {
-		useEffect(() => {
-	    window.scrollTo(0, 0);
-	  }, []);
+	const performances = useSelector( state => state.pools.performances )
+	const dispatch = useDispatch()
 
-    return (
-      <div className="container-fluid mh-100">
-	      <div className="row mb-5 h-100">
-	        <div className='col'>
-	        </div>
-	        <div className="col-lg-10 mr-auto ml-auto">
-	        	Pools Gauge
-	        </div>
-	        <div className='col'></div>
-	      </div>
-	    </div>
-    )
+	useEffect(() => {
+    window.scrollTo(0, 0);
+    dispatch(getPoolPerformances(20))
+  }, []);
+
+  return (
+    <div className="container-fluid mh-100">
+      <div className="row mb-5 h-100">
+        <div className='col'>
+        </div>
+        <div className="col-lg-10 mr-auto ml-auto">
+        	{performances.map(performance => <p>{performance.blocks_delta_pc}</p>)}
+        </div>
+        <div className='col'></div>
+      </div>
+    </div>
+  )
 }
 
 export default PoolsGauge

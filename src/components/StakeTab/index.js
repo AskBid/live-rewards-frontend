ab4f381874e2f83499aaa8a49ac2865ea5b43536
Sergio Marelli <sergio.mrll@gmail.com>
2021-05-04 00:19:35 +0100

    stakeTab goes skeleton also if loading

--- src/components/StakeTab/index.js
+++ src/components/StakeTab/index.js
@@ -28,6 +28,7 @@ const StakeTab = ({stake, tabType}) => {
   const user = useSelector(state => state.sessions.user)
   const currency = useSelector(state => state.sessions.currency.symbol)
   const price = useSelector(state => state.sessions.currency.price)
+  const loading = useSelector(state => state.epoch_stakes.loading)
 
   const stake_address_id = stake.stake_address && stake.stake_address.id
   const stake_address_view = stake.stake_address && stake.stake_address.view
@@ -102,14 +103,14 @@ const StakeTab = ({stake, tabType}) => {
     return (
       <div className='d-flex flex-column justify-content-start align-items-start p-2'
         style={{minWidth:'55px',background:'none'}}>
-        <DeleteBtn type='Submit' className='w-100 shadow-sm text-center d-flex flex-column justify-content-center align-items-center' disabled={!stake.id}>
+        <DeleteBtn type='Submit' className='w-100 shadow-sm text-center d-flex flex-column justify-content-center align-items-center' disabled={!stake.id || loading}>
           <b className='position-absolute' style={{fontSize:'0.9em', top:'-1.5em'}}>Delete!</b>
           <CloseIcon size={10}/>
         </DeleteBtn>
         
         <Link to={`/pool-compare/users/${user}/epoch_stakes/${stake.id}`} className='w-100 h-100 mt-2'>
           <OverlayTrigger placement='top' overlay={compareTip}>     
-            <FuncBtn type='Submit' className='w-100 h-100 shadow-sm' disabled={!stake.id}>
+            <FuncBtn type='Submit' className='w-100 h-100 shadow-sm' disabled={!stake.id || loading}>
               <PoolIcon size={20}/>
             </FuncBtn>
           </OverlayTrigger>
@@ -117,7 +118,7 @@ const StakeTab = ({stake, tabType}) => {
         
         <Link to={`/delegation-flows/epochs/${stake.epoch_no}/pools/${ticker}`} className='w-100 h-100 mt-2 shadow-sm'> 
           <OverlayTrigger placement='top' overlay={delegationFlowTip}>   
-          <FuncBtn type='Submit' className='w-100 h-100 shadow-sm' disabled={!stake.id}>
+          <FuncBtn type='Submit' className='w-100 h-100 shadow-sm' disabled={!stake.id || loading}>
             <DeleFlowIcon size={20}/>
           </FuncBtn>
           </OverlayTrigger>
@@ -185,19 +186,19 @@ const StakeTab = ({stake, tabType}) => {
         <div className='col d-flex flex-row flex-wrap m-0 p-1'>
           <div className='col text-dark text-center m-0 p-0 mt-auto mb-auto mr-auto ml-auto' style={{ minWidth:'7em'}}>
             <h2 className='text-dark mt-auto mb-auto mr-auto ml-auto'>
-              {ticker || <Skeleton />}
+              {(ticker && !loading) ? ticker : <Skeleton />}
             </h2>
           </div>
           <div className='container col mb-auto mt-auto mr-1'>
             <div className='ml-auto mb-auto mt-auto'>
               
-              {stake.id ? <RewardsRow rewards={rewards} /> : <h3><Skeleton /></h3>}
+              {(stake.id && !loading) ? <RewardsRow rewards={rewards} /> : <h3><Skeleton /></h3>}
 
               <ValueRow 
                 label={ 'stake:' }
                 symbol={symbols[currency]}
                 value={ stake.amount ? numeral(parseInt(stake.amount*price) / 1000000).format('0,0') : undefined }
-                skeleton={!stake.id}
+                skeleton={!stake.id || loading}
               />
 
               { pool_hash_size ?
@@ -205,7 +206,7 @@ const StakeTab = ({stake, tabType}) => {
                   label={ 'pool size:' }
                   symbol={symbols[currency]}
                   value={ pool_hash_size ? numeral(pool_hash_size*price).format('0,0') : undefined}
-                  skeleton={!stake.id}
+                  skeleton={!stake.id || loading}
                 /> : null
               }
 
@@ -221,14 +222,14 @@ const StakeTab = ({stake, tabType}) => {
                     <strong className='text-monospace'>{stake.blocks}</strong>
                   </OverlayTrigger>
                 }
-                skeleton={!stake.id}
+                skeleton={!stake.id || loading}
               />
               
               <ValueRow 
                 label={ 'ROS:' }
                 symbol={numeral(calcROS(stake.amount/1000000, stake.calc_rewards)).format('0,0.00')}
                 value={"%"}
-                skeleton={!stake.id}
+                skeleton={!stake.id || loading}
               />
               
 

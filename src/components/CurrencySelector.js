6f8739e15a02fbae868e8ddc905611ccb5ffc60d
Sergio Marelli <sergio.mrll@gmail.com>
2021-05-03 16:40:35 +0100

    removed old code

--- src/components/CurrencySelector.js
+++ src/components/CurrencySelector.js
@@ -34,43 +34,6 @@ const CurrencySelector = () => {
           <Dropdown.Item disabled>Ξ - Ethereum</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
-
-      {/*<button 
-        className={`${currency === 'ada' ? 'selected-currency' : null} buttonsbar shadow-sm border-0 text-nowrap rounded-pill m-1`}
-        onClick={() => dispatch({type: REQUEST_PRICE_SUCCESS, payload: {symbol: 'ada', price: 1}})}
-        id='ada'>
-        ₳
-      </button>
-      <button 
-        className={`${currency === 'usd' ? 'selected-currency' : null} buttonsbar shadow-sm border-0 text-nowrap rounded-pill m-1`}
-        onClick={switchCurrency}
-        id='usd'>
-        $
-      </button>
-      <button 
-        className={`${currency === 'eur' ? 'selected-currency' : null} buttonsbar shadow-sm border-0 text-nowrap rounded-pill m-1`}
-        onClick={switchCurrency}
-        id='eur'>
-        €
-      </button>
-      <button 
-        className={`${currency === 'gbp' ? 'selected-currency' : null} buttonsbar shadow-sm border-0 text-nowrap rounded-pill m-1`}
-        onClick={switchCurrency}
-        id='gbp'>
-        £
-      </button>
-      <button 
-        className={`${currency === 'jpy' ? 'selected-currency' : null} buttonsbar shadow-sm border-0 text-nowrap rounded-pill m-1`}
-        onClick={switchCurrency}
-        id='jpy'>
-        ¥
-      </button>
-      <button 
-        className='buttonsbar border-0 text-nowrap rounded ml-1 mr-1 mb-0 w-auto'
-        onClick={switchCurrency}
-        id='btc'>
-        ฿
-      </button>*/}
     </div>
   )
 }

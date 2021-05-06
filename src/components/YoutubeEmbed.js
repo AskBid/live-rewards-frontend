2de1f46beca3d2caa7f566411880898ab9a120af
Sergio Marelli <sergio.mrll@gmail.com>
2021-05-06 23:16:47 +0100

    prepared intro page embedded video

---
+++ src/components/YoutubeEmbed.js
@@ -0,0 +1,25 @@
+//https://youtu.be/3MhCjNFLlWE
+
+import React from "react";
+import PropTypes from "prop-types";
+
+const YoutubeEmbed = ({ embedId }) => (
+  <div className="video-responsive">
+    <iframe
+      width="853"
+      height="480"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+      className='border border-dark rounded'
+    />
+  </div>
+);
+
+YoutubeEmbed.propTypes = {
+  embedId: PropTypes.string.isRequired
+};
+
+export default YoutubeEmbed;
--- src/pages/howto.js
+++ src/pages/howto.js
@@ -4,6 +4,7 @@ import Image from '../assets/cardanoscan.png';
 import { Link } from 'react-router-dom'
 import IntroSchema from '../components/IntroSchema';
 import {current_epoch} from '../helpers/epoch_helpers'
+import YoutubeEmbed from '../components/YoutubeEmbed'
 
 function Howto({history}) {
 
@@ -17,22 +18,27 @@ function Howto({history}) {
     <div className="container">
       <div className="row mb-5">
         <div className='col'></div>
-        <div className="col-lg-6 mt-5 mb-5 text-dark">
-          <h2 className='text-dark'>All you need is your Stake Address</h2>
-          <br/>
-          <p>Your wallet can have many receiving addresses to which ADA can be sent. Those addresses usually start with <i>addr1.</i></p>
-          <p>There is also a unique Stake Address that your wallet uses to delegate to staking pools. This address starts with <i>stake1.</i></p>
-
-          <p>To find your Stake Address, get any of your receiveing addresses (starting <i>addr1...</i>) and look it up on <a className='hardlink' href="https://cardanoscan.io/" target="_blank" rel="noreferrer">cardanoscan.io</a>.</p> 
-          <p>In the <i>Controlled Stake Section</i>, of the cardanoscan.io page, you will find the BECH32 Stake Address needed to track your Rewards with <Link to={`/live-rewards`} className='hardlink'>Live-Rewards</Link>.</p>
-          <br/>
-          <div className='container center'>
-          <img className='text-center' src={Image} alt="cardanoscan.io stake address location." style={{width:'500px'}}/>
+        <div className="col-lg-10 mt-5 mb-5 text-dark">
+          <div className="App">
+            <h2 className='text-dark'>All you need is a wallet address</h2>
+            <br/>
+            <p>In this video we show you how to get an address from your <b>Yoroi</b> app and use the <i>Live-Rewards</i></p>
+            <br/>
+            <br/>
+            <YoutubeEmbed embedId="3MhCjNFLlWE" />
           </div>
           <br/>
-          <p>Add your stake address in <Link to={`/live-rewards`} className='hardlink'>Live-Rewards</Link> and magically you'll be able to see the rewards of the last three epochs, including rewards you have not received yet.</p>
+          <br/>
+          <p>Your wallet can have many receiving addresses to which ADA can be sent. Those addresses usually start with <b><i>addr1.</i></b></p>
+          <p>There is also a unique Stake Address that your wallet uses to delegate to staking pools. This address starts with <b><i>stake1.</i></b></p>
 
+          <p>You can enter any of those, <i>Live-Rewards</i> will show your <b>Cardano</b> delegation and the rewards that you have been gaining in the last 3 epochs.</p>
           <p>Each epoch stake tab has a button that will allow you to compare your delegation with different Pools. With that you can compare your pool performance with that of other pools.</p>
+          <br/>
+          <br/>
+          <p>In the schema below you can see how usually when you send your delegastion to a Pool, it will take you 4/5 epochs to receive your ADA rewards. That can be up to 20 days of waiting! with <i>Live-Rewards</i> you can now know your <b>Cardano</b> rewards as soon as your delegation is active after only 2 epochs.</p>
+          <br/>
+
           <div className="col-sm mt-3 mb-5">
             <IntroSchema 
               opacity={'40%'} 
